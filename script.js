7bfdcd3d1e513b579899b558f034d846dfbc03d8
mr-abdellah <belkaid.abdulah@gmail.com>
2022-05-09 20:14:07 +0100

    fixing linters issues

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const mobileNav = document.querySelector('.m-navbar');
 const hamburgerCloseBtn = document.getElementById('close');
 const mobileLink = document.querySelectorAll('.m-navbar-link');
 
-hamburgerOpenBtn.addEventListener("click", function open () {
-    mobileNav.style.display = "flex";
-    mobileNav.style.top = "0";
+hamburgerOpenBtn.addEventListener('click', function open () {
+  mobileNav.style.display = 'flex';
+  mobileNav.style.top = '0';
 });
 
-hamburgerCloseBtn.addEventListener("click", function close(){
-    mobileNav.style.display = "none";
-    mobileNav.style.top = "-100%";
+hamburgerCloseBtn.addEventListener('click', function close(){
+  mobileNav.style.display = 'none';
+  mobileNav.style.top = '-100%';
 });
 
 mobileLink.forEach(link => {
-    link.addEventListener("click", function closeContent() {
-        mobileNav.style.display = "none";
-        mobileNav.style.top = "-100%";
-    })
+  link.addEventListener('click', function closeContent() {
+    mobileNav.style.display = 'none';
+    mobileNav.style.top = '-100%';
+})
 });
