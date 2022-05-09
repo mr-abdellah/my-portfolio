6e60c03c56280cfc0384bffc5074f5113bd63624
mr-abdellah <belkaid.abdulah@gmail.com>
2022-05-09 20:08:48 +0100

    fixing javascript issues

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const hamburgerOpenBtn = document.getElementById("open");
-const mobileNav = document.querySelector(".m-navbar");
-const hamburgerCloseBtn = document.getElementById("close");
-const mobileLink = document.querySelectorAll(".m-navbar-link");
+const hamburgerOpenBtn = document.getElementById('open');
+const mobileNav = document.querySelector('.m-navbar');
+const hamburgerCloseBtn = document.getElementById('close');
+const mobileLink = document.querySelectorAll('.m-navbar-link');
 
-hamburgerOpenBtn.addEventListener("click", open);
-hamburgerCloseBtn.addEventListener("click", close);
-
-function open() {
+hamburgerOpenBtn.addEventListener("click", function open () {
     mobileNav.style.display = "flex";
     mobileNav.style.top = "0";
-}
-
-function close (){
-    mobileNav.style.display = "none";
-    mobileNav.style.top = "-100%";
-}
-
-
-mobileLink.forEach(link => {
-    link.addEventListener("click", closeContent)
 });
 
-function closeContent() {
+hamburgerCloseBtn.addEventListener("click", function close(){
     mobileNav.style.display = "none";
     mobileNav.style.top = "-100%";
-}
+});
+
+mobileLink.forEach(link => {
+    link.addEventListener("click", function closeContent() {
+        mobileNav.style.display = "none";
+        mobileNav.style.top = "-100%";
+    })
+});
