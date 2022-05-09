0b1934c225fc1aebe028ccc2a1b46329b5ef0997
mr-abdellah <belkaid.abdulah@gmail.com>
2022-05-09 20:52:56 +0100

    fixing js linters errors

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ hamburgerCloseBtn.addEventListener('click', () => {
 });
 
 mobileLink.forEach(link => {
-  link.addEventListener('click', (link) => {
+  link.addEventListener('click', (param) => {
     mobileNav.style.display = 'none';
     mobileNav.style.top = '-100%';
   });
