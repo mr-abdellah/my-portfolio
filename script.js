378c8930faffba090ed9560f07934c3b20293941
BHDHakim <hakim.bhd@gmail.com>
2022-05-09 13:39:55 +0100

    Add close feature

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const hamburgerOpenBtn = document.getElementById("open");
 const mobileNav = document.querySelector(".m-navbar");
+const hamburgerCloseBtn = document.getElementById("close");
 
 hamburgerOpenBtn.addEventListener("click", open);
+hamburgerCloseBtn.addEventListener("click", close);
+
 function open() {
     mobileNav.style.display = "flex";
     mobileNav.style.top = "0";
     console.log("open button clicked");
+}
+
+function close (){
+    mobileNav.style.display = "none";
+    mobileNav.style.top = "-100%";
+    console.log("close buttonclicked");
 }
