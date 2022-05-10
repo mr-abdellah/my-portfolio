2b820480f6be828924224d1540cae55427f7a382
mr-abdellah <belkaid.abdulah@gmail.com>
2022-05-10 14:04:50 +0100

    Adding a function that generate project

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ function closeContent() {
 }
 
 mobileLink.forEach((link) => { link.addEventListener('click', closeContent); });
+
+let projectsContainer = document.querySelector('.my-recent-works');
+
+function generatProject (name,description, image, technologies,liveVersion,source) {
+  return `
+  <div class="project">
+  <div class="work-background" style="background : url(${image})"></div>
+      <h3 class="work-title">${name}</h3>
+      <div class="categories">
+        <ul>
+          ${
+            technologies.map(technology =>{
+              return `<li>${technology}</li>`
+            })
+          }
+        </ul>
+      </div>
+      <button type="submit">See Project</button>
+  </div>
+</div>
+  `
+}
+let myProject = generatProject("project 1", null , null, ["Ruby on rails", "css", "JavaScript"]);
+projectsContainer.innerHTML = myProject;
